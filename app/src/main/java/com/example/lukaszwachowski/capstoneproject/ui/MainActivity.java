35f07de8ebbeb5a5613804c9e15c90c644062056
Łukasz Wachowski <lwach87@gmail.com>
2018-08-10 17:30:59 +0200

    PagerAdapter created

--- app/src/main/java/com/example/lukaszwachowski/capstoneproject/ui/MainActivity.java
+++ app/src/main/java/com/example/lukaszwachowski/capstoneproject/ui/MainActivity.java
@@ -1,11 +1,10 @@
 package com.example.lukaszwachowski.capstoneproject.ui;
 
-import android.arch.lifecycle.ViewModelProviders;
 import android.os.Bundle;
-import android.support.design.widget.Snackbar;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 import android.view.ViewGroup;
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -13,27 +12,24 @@ import com.example.lukaszwachowski.capstoneproject.EarthquakeApp;
 import com.example.lukaszwachowski.capstoneproject.R;
 import com.example.lukaszwachowski.capstoneproject.di.components.DaggerMainActivityComponent;
 import com.example.lukaszwachowski.capstoneproject.di.modules.MainActivityModule;
-import io.reactivex.android.schedulers.AndroidSchedulers;
-import io.reactivex.disposables.CompositeDisposable;
-import io.reactivex.schedulers.Schedulers;
 import javax.inject.Inject;
 
 public class MainActivity extends AppCompatActivity {
 
   @Inject
-  ListAdapter listAdapter;
+  PagerAdapter pagerAdapter;
 
-  @Inject
-  MainActivityViewModelFactory factory;
+  @BindView(R.id.toolbar)
+  Toolbar toolbar;
 
-  @BindView(R.id.recycler_view)
-  RecyclerView recyclerView;
+  @BindView(R.id.view_pager)
+  ViewPager viewPager;
 
-  @BindView(R.id.main_activity)
+  @BindView(R.id.tab_layout)
+  TabLayout tabLayout;
+
+  @BindView(R.id.activity_main)
   ViewGroup rootView;
-
-  private MainActivityViewModel viewModel;
-  private CompositeDisposable disposable = new CompositeDisposable();
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -46,26 +42,8 @@ public class MainActivity extends AppCompatActivity {
         .mainActivityModule(new MainActivityModule(this))
         .build().inject(this);
 
-    viewModel = ViewModelProviders.of(this, factory).get(MainActivityViewModel.class);
-
-    getDataFromDatabase();
-
-    recyclerView.setLayoutManager(new LinearLayoutManager(this));
-    recyclerView.setAdapter(listAdapter);
-  }
-
-  private void getDataFromDatabase() {
-    disposable.add(viewModel.getFeatures()
-        .subscribeOn(Schedulers.io())
-        .observeOn(AndroidSchedulers.mainThread())
-        .subscribe(features -> listAdapter.swapData(features), throwable ->
-            Snackbar.make(rootView, throwable.getLocalizedMessage(), Snackbar.LENGTH_SHORT)
-                .show()));
-  }
-
-  @Override
-  protected void onDestroy() {
-    super.onDestroy();
-    disposable.clear();
+    setSupportActionBar(toolbar);
+    viewPager.setAdapter(pagerAdapter);
+    tabLayout.setupWithViewPager(viewPager);
   }
 }
